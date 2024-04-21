c3f244044ddb1b44aa448541cb617ec25ce7106e
DoniaAli77 <donia.ali.7799@gmail.com>
2024-04-21 13:06:47 +0200

    authentication

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (ctx) => IdeasProvider()), 
       ], 
       child: MaterialApp( 
+        debugShowCheckedModeBanner: false,
           title: 'Flutter Demo', 
           theme: ThemeData( 
             primarySwatch: Colors.deepOrange, 
